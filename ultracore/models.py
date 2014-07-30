bddf0d2fe1db517f8976fa08634c0d95758c9aaa
Alejandro Varas <alej0varas@gmail.com>
2014-07-30 14:54:44 -0400

    Added contact form

--- ultracore/models.py
+++ ultracore/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
+    InlinePanel
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
 
-# Create your models here.
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+
+FormPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('intro', classname="full"),
+    InlinePanel(FormPage, 'form_fields', label="Form fields"),
+    FieldPanel('thank_you_text', classname="full"),
+    MultiFieldPanel([
+        FieldPanel('to_address', classname="full"),
+        FieldPanel('from_address', classname="full"),
+        FieldPanel('subject', classname="full"),
+    ], "Email")
+]
