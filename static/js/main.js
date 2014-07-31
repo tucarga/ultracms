743978fdc1b36003b99c23a56e7cacd2075aaa4c
Javier Vasquez <s0wkete@gmail.com>
2014-07-31 12:04:00 -0400

    Loop de servicios

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
+// Init foudnation library
+$(document).foundation();
+
+// Function of the site
+$(document).ready(function() {
+    $(".overlay").css("width", $(".loop-servicios").width());
     /* Initialise bxSlider */
     $('.bxslider').bxSlider({
         captions: true
     });
-
     //Apply img-thumbnail class to body-content images
     $('.body-content img').addClass("img-thumbnail");
 });
+
+// Resize function for the proyect hover
+$(window).resize(function() {
+    $(".overlay").css("width", $(".loop-servicios img").width());
+});
+
